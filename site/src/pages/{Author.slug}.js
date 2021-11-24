beee3417c3e4301a148ccc77d40ba9eb6ba98023
Random Guy <leekhoi5319990@gmail.com>
2021-11-24 12:09:41 +0700

    Automatically create pages with custom data

---
+++ site/src/pages/{Author.slug}.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { graphql, Link } from 'gatsby';
+import slugify from 'slugify';
+
+export const query = graphql`
+	query AuthorPage($id: String!) {
+		# {$id} is the param
+		author(id: { eq: $id }) {
+			name
+			books {
+				id
+				name
+				series
+				seriesOrder
+			}
+		}
+	}
+`;
+
+const sortAndExtendBooks = (books) => {
+	return books
+		.sort((a, b) => a.seriesOrder - b.seriesOrder)
+		.map((book) => {
+			const series = book.series
+				? `(${book.series}, book ${book.seriesOrder})`
+				: '';
+			const displayName = `${book.name} ${series}`;
+			const bookSlug = slugify(book.name, { lower: true });
+
+			let path;
+			if (book.series !== null) {
+				const seriesSlug = slugify(book.series, { lower: true });
+				path = `/book/${seriesSlug}/${bookSlug}`;
+			}
+			path = `/book/${bookSlug}`;
+
+			return { displayName, path };
+		});
+};
+
+const AuthorPage = ({ data }) => {
+	// {data} is the destructured prop of query's result, and it HAS TO BE used as-is because it's reserved by Gatsby
+	const author = data.author;
+	const books = sortAndExtendBooks(author.books);
+
+	return (
+		<div>
+			<h1>{author.name}</h1>
+			<p>Books by {author.name}:</p>
+			<ul>
+				{books.map((book) => (
+					<li key={book.id}>
+						<Link to={book.path}>{book.displayName}</Link>
+					</li>
+				))}
+			</ul>
+			<Link to="/authors">&larr; back to all authors</Link>
+		</div>
+	);
+};
+
+export default AuthorPage;
