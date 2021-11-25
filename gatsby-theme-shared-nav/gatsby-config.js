0fdc054e7e48b73229e0c1f906028c0053d1c787
Random Guy <leekhoi5319990@gmail.com>
2021-11-25 18:03:41 +0700

    Add a common Horizontal Navbar for pages

--- gatsby-theme-shared-nav/gatsby-config.js
+++ gatsby-theme-shared-nav/gatsby-config.js
@@ -1,1 +1,21 @@
-module.exports = {};
+module.exports = {
+	siteMetadata: {
+		title: 'Gatsby Theme Shared Nav',
+		navItems: [
+			{
+				label: 'Home',
+				path: '/',
+			},
+		],
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-plugin-layout',
+			options: {
+				component: require.resolve(
+					__dirname + '/src/components/layout.js',
+				), // {__dirname} is to make sure it uses gatsby-theme-shared-nav's theme
+			},
+		},
+	],
+};
