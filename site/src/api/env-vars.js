85752948eabf5a76a1061ed1b5699d6dcafc45fb
Random Guy <leekhoi5319990@gmail.com>
2021-11-27 18:22:32 +0700

    .

--- site/src/api/env-vars.js
+++ site/src/api/env-vars.js
@@ -6,3 +6,4 @@ export default function handler(_req, res) {
 }
 
 // serverless functions give the privilege to work with private values
+// visit: https://github.com/netlify/explorers to see how they work IRL
